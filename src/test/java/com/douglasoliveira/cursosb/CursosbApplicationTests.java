c6c2cd764692f843f50b5ec1807a9e52b6a29469
Goga <2doug077@gmail.com>
2021-05-10 09:13:30 -0300

    created project

---
+++ src/test/java/com/douglasoliveira/cursosb/CursosbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.douglasoliveira.cursosb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursosbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
