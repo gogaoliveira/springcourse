51c9d5752e44d64ad57ded356c20dca36680f60d
Goga <2doug077@gmail.com>
2021-05-10 14:14:29 -0300

    intance

--- src/main/java/com/douglasoliveira/cursosb/CursosbApplication.java
+++ src/main/java/com/douglasoliveira/cursosb/CursosbApplication.java
@@ -1,13 +1,33 @@
 package com.douglasoliveira.cursosb;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class CursosbApplication {
+import com.douglasoliveira.cursosb.domain.Categoria;
+import com.douglasoliveira.cursosb.repositories.CategoriaRepository;
 
+@SpringBootApplication
+public class CursosbApplication implements CommandLineRunner {
+
+	@Autowired
+	private CategoriaRepository categoriaRespository;
 	public static void main(String[] args) {
 		SpringApplication.run(CursosbApplication.class, args);
+		
+		
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		
+		Categoria cat1 =  new Categoria(null, "Informática");
+		Categoria cat2 =  new Categoria(null, "Escritório");
+		
+		categoriaRespository.saveAll(Arrays.asList(cat1, cat2));
 	}
 
 }
