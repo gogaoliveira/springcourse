fc8795599ffb1c21ad8e1020b3bd26eaafe93fa3
Goga <2doug077@gmail.com>
2021-05-11 14:40:07 -0300

    deletando uma categoria

--- src/main/java/com/douglasoliveira/cursosb/services/CategoriaService.java
+++ src/main/java/com/douglasoliveira/cursosb/services/CategoriaService.java
@@ -3,10 +3,12 @@ package com.douglasoliveira.cursosb.services;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 
 import com.douglasoliveira.cursosb.domain.Categoria;
 import com.douglasoliveira.cursosb.repositories.CategoriaRepository;
+import com.douglasoliveira.cursosb.services.exceptions.DataIntegrityException;
 import com.douglasoliveira.cursosb.services.exceptions.ObjectNotFoundException;
 
 @Service
@@ -29,5 +31,14 @@ public class CategoriaService {
 	public Categoria update(Categoria obj) {
 		find(obj.getId());
 		return repo.save(obj);
+	}
+	
+	public void delete(Integer id) {
+		find(id);
+		try {
+			repo.deleteById(id);
+		}catch(DataIntegrityViolationException e) {
+			throw new DataIntegrityException("Não é possivel excluir uma categoria que possui produtos");
+		}
 	}
 }
