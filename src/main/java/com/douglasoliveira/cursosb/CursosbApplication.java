74e03ddade96d81024b6086bab8bc095b9078241
Goga <2doug077@gmail.com>
2021-05-11 09:11:55 -0300

    pedido, Estado pagamento e pagamento

--- src/main/java/com/douglasoliveira/cursosb/CursosbApplication.java
+++ src/main/java/com/douglasoliveira/cursosb/CursosbApplication.java
@@ -1,5 +1,6 @@
 package com.douglasoliveira.cursosb;
 
+import java.text.SimpleDateFormat;
 import java.util.Arrays;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,13 +13,20 @@ import com.douglasoliveira.cursosb.domain.Cidade;
 import com.douglasoliveira.cursosb.domain.Cliente;
 import com.douglasoliveira.cursosb.domain.Endereco;
 import com.douglasoliveira.cursosb.domain.Estado;
+import com.douglasoliveira.cursosb.domain.Pagamento;
+import com.douglasoliveira.cursosb.domain.PagamentoComBoleto;
+import com.douglasoliveira.cursosb.domain.PagamentoComCartao;
+import com.douglasoliveira.cursosb.domain.Pedido;
 import com.douglasoliveira.cursosb.domain.Produto;
+import com.douglasoliveira.cursosb.domain.enums.EstadoPagamento;
 import com.douglasoliveira.cursosb.domain.enums.TipoCliente;
 import com.douglasoliveira.cursosb.repositories.CategoriaRepository;
 import com.douglasoliveira.cursosb.repositories.CidadeRepository;
 import com.douglasoliveira.cursosb.repositories.ClienteRepository;
 import com.douglasoliveira.cursosb.repositories.EnderecoRepository;
 import com.douglasoliveira.cursosb.repositories.EstadoRepository;
+import com.douglasoliveira.cursosb.repositories.PagamentoRepository;
+import com.douglasoliveira.cursosb.repositories.PedidoRepository;
 import com.douglasoliveira.cursosb.repositories.ProdutoRepository;
 
 @SpringBootApplication
@@ -41,6 +49,12 @@ public class CursosbApplication implements CommandLineRunner {
 	
 	@Autowired
 	private EnderecoRepository enderecoRepository;
+	
+	@Autowired
+	private PedidoRepository pedidoRepository;
+	
+	@Autowired
+	private PagamentoRepository pagamentoRepository;
 	
 	public static void main(String[] args) {
 		SpringApplication.run(CursosbApplication.class, args);
@@ -92,6 +106,24 @@ public class CursosbApplication implements CommandLineRunner {
 		
 		clienteRepository.saveAll(Arrays.asList(cli1));
 		enderecoRepository.saveAll(Arrays.asList(e1, e2));
+		
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy HH:mm");
+		
+		Pedido ped1 = new Pedido(null, sdf.parse("30/09/2017 10:32"), cli1, e1);
+		Pedido ped2 = new Pedido(null, sdf.parse("10/10/2017 19:35"), cli1, e2);
+		
+		Pagamento pagto1 = new PagamentoComCartao(null, EstadoPagamento.QUITADO, ped1, 6);
+		ped1.setPagamento(pagto1);
+		
+		Pagamento pagto2 = new PagamentoComBoleto(null, EstadoPagamento.PENDENTE, ped2, sdf.parse("20/10/2017 00:00"), null);
+		ped2.setPagamento(pagto2);
+		
+		cli1.getPedidos().addAll(Arrays.asList(ped1, ped2));
+				
+		pedidoRepository.saveAll(Arrays.asList(ped1, ped2));
+		pagamentoRepository.saveAll(Arrays.asList(pagto1, pagto2));
+
+		
 	}
 
 }
--- src/main/java/com/douglasoliveira/cursosb/domain/Cliente.java
+++ src/main/java/com/douglasoliveira/cursosb/domain/Cliente.java
@@ -18,9 +18,9 @@ import com.douglasoliveira.cursosb.domain.enums.TipoCliente;
 import com.fasterxml.jackson.annotation.JsonManagedReference;
 
 @Entity
-public class Cliente implements Serializable{
+public class Cliente implements Serializable {
 	private static final long serialVersionUID = 1L;
-	
+
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
@@ -28,16 +28,20 @@ public class Cliente implements Serializable{
 	private String email;
 	private String cpfOuCnpj;
 	private Integer tipo;
-	
+
 	@JsonManagedReference
 	@OneToMany(mappedBy = "cliente")
 	private List<Endereco> enderecos = new ArrayList<>();
-	
+
 	@ElementCollection
 	@CollectionTable(name = "telefone")
 	private Set<String> telefones = new HashSet<>();
-	
-	public Cliente() {}
+
+	@OneToMany(mappedBy = "cliente")
+	private List<Pedido> pedidos = new ArrayList<>();
+
+	public Cliente() {
+	}
 
 	public Cliente(Integer id, String nome, String email, String cpfOuCnpj, TipoCliente tipo) {
 		super();
@@ -104,6 +108,14 @@ public class Cliente implements Serializable{
 		this.telefones = telefones;
 	}
 
+	public List<Pedido> getPedidos() {
+		return pedidos;
+	}
+
+	public void setPedidos(List<Pedido> pedidos) {
+		this.pedidos = pedidos;
+	}
+
 	@Override
 	public int hashCode() {
 		final int prime = 31;
@@ -128,7 +140,5 @@ public class Cliente implements Serializable{
 			return false;
 		return true;
 	}
-	
-	
-	
+
 }
