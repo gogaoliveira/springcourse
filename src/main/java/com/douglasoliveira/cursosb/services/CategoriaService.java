c0ba8a55d372cda1c214dd0e99f2c40dd1533527
Goga <2doug077@gmail.com>
2021-05-11 16:06:44 -0300

    listando todas categorias com DTO

--- src/main/java/com/douglasoliveira/cursosb/services/CategoriaService.java
+++ src/main/java/com/douglasoliveira/cursosb/services/CategoriaService.java
@@ -1,5 +1,6 @@
 package com.douglasoliveira.cursosb.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -38,7 +39,11 @@ public class CategoriaService {
 		try {
 			repo.deleteById(id);
 		}catch(DataIntegrityViolationException e) {
-			throw new DataIntegrityException("Não é possivel excluir uma categoria que possui produtos");
+			throw new DataIntegrityException("Não é possível excluir uma categoria que possui produtos");
 		}
+	}
+	
+	public List<Categoria> findAll(){
+		return repo.findAll();
 	}
 }
