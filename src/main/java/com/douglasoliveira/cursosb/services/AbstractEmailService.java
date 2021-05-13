0574a39406703497f7d9c408ed84ffa7c51d7d15
Goga <2doug077@gmail.com>
2021-05-13 15:23:19 -0300

    email html

--- src/main/java/com/douglasoliveira/cursosb/services/AbstractEmailService.java
+++ src/main/java/com/douglasoliveira/cursosb/services/AbstractEmailService.java
@@ -2,16 +2,30 @@ package com.douglasoliveira.cursosb.services;
 
 import java.util.Date;
 
+import javax.mail.MessagingException;
+import javax.mail.internet.MimeMessage;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.context.Context;
 
 import com.douglasoliveira.cursosb.domain.Pedido;
 
-public abstract class AbstractEmailService implements EmailService{
+public abstract class AbstractEmailService implements EmailService {
+
+	@Autowired
+	private JavaMailSender javaMailSender;
 	
+	@Autowired
+	private TemplateEngine templeteEngine;
+
 	@Value("${default.sender}")
 	private String sender;
-	
+
 	@Override
 	public void sendOrderConfirmationEmail(Pedido obj) {
 		SimpleMailMessage sm = prepareSimpleMailMessageFromPedido(obj);
@@ -26,6 +40,34 @@ public abstract class AbstractEmailService implements EmailService{
 		sm.setSentDate(new Date(System.currentTimeMillis()));
 		sm.setText(obj.toString());
 		return sm;
+	}
+
+	protected String htmlFromTemplatePedido(Pedido obj) {
+		Context context = new Context();
+		context.setVariable("pedido", obj);
+		return templeteEngine.process("email/confirmacaoPedido", context);
+	}
+	
+	@Override
+	public void sendOrderConfirmationHtmlEmail(Pedido obj) {
+		try {
+			MimeMessage mm = prepareMimeMessageFromPedido(obj);
+			sendHtmlEmail(mm);
+		} catch (MessagingException e) {
+			sendOrderConfirmationEmail(obj);
+		}
+		
+	}
+
+	 protected MimeMessage prepareMimeMessageFromPedido(Pedido obj) throws MessagingException {
+		MimeMessage mimeMessage = javaMailSender.createMimeMessage();
+		MimeMessageHelper mmh = new MimeMessageHelper(mimeMessage, true);
+		mmh.setTo(obj.getCliente().getEmail());
+		mmh.setFrom(sender);
+		mmh.setSubject("Pedido Confirmado! Código: "+obj.getId());
+		mmh.setSentDate(new Date(System.currentTimeMillis()));
+		mmh.setText(htmlFromTemplatePedido(obj),true);
+		return mimeMessage;
 	}
 
 }
--- src/main/java/com/douglasoliveira/cursosb/services/EmailService.java
+++ src/main/java/com/douglasoliveira/cursosb/services/EmailService.java
@@ -1,5 +1,7 @@
 package com.douglasoliveira.cursosb.services;
 
+import javax.mail.internet.MimeMessage;
+
 import org.springframework.mail.SimpleMailMessage;
 
 import com.douglasoliveira.cursosb.domain.Pedido;
@@ -9,5 +11,10 @@ public interface EmailService {
 	void sendOrderConfirmationEmail(Pedido obj);
 	
 	void sendEmail(SimpleMailMessage msg);
+	
+	void sendOrderConfirmationHtmlEmail(Pedido obj);
+	
+	void sendHtmlEmail(MimeMessage msg);
+
 }
 
