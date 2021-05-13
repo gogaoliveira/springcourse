0952c4d2c83aed2f78f45b1b52e15116dc143ca1
Goga <2doug077@gmail.com>
2021-05-13 19:03:09 -0300

    implementando autenticação e geração do token com jwt

---
+++ src/main/java/com/douglasoliveira/cursosb/services/UserDetailsServiceImpl.java
@@ -0,0 +1,29 @@
+package com.douglasoliveira.cursosb.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import com.douglasoliveira.cursosb.domain.Cliente;
+import com.douglasoliveira.cursosb.repositories.ClienteRepository;
+import com.douglasoliveira.cursosb.security.UserSS;
+
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService{
+
+	@Autowired
+	private ClienteRepository repo;
+	
+	@Override
+	public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+		Cliente cli = repo.findByEmail(email);
+		if (cli == null ) {
+			throw new UsernameNotFoundException(email);
+		}
+		
+		return new UserSS(cli.getId(), cli.getEmail(), cli.getSenha(), cli.getPerfis());
+	}
+
+}
