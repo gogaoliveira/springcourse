90c3f8091ed7e2547a19a7cd9bf9e6c43229c23d
Goga <2doug077@gmail.com>
2021-05-14 17:54:47 -0300

    eniando imagem via endpoint

--- src/main/java/com/douglasoliveira/cursosb/CursosbApplication.java
+++ src/main/java/com/douglasoliveira/cursosb/CursosbApplication.java
@@ -1,17 +1,13 @@
 package com.douglasoliveira.cursosb;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import com.douglasoliveira.cursosb.services.S3Service;
-
 @SpringBootApplication
 public class CursosbApplication implements CommandLineRunner {
 
-	@Autowired
-	private S3Service s3Service;
+
 	
 	public static void main(String[] args) {
 		SpringApplication.run(CursosbApplication.class, args);
@@ -20,7 +16,6 @@ public class CursosbApplication implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
-		s3Service.uploadFile("C:\\Cursos\\Spring Boot\\ws_spring_boot\\doug.png");
 	}
 
 }
