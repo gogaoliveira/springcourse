19573a79c6daa9fe68615b6b9ee3667fc063f9ec
Goga <2doug077@gmail.com>
2021-05-17 11:48:46 -0300

    ajustando tamanho da imagem

--- src/main/java/com/douglasoliveira/cursosb/services/ClienteService.java
+++ src/main/java/com/douglasoliveira/cursosb/services/ClienteService.java
@@ -46,13 +46,13 @@ public class ClienteService {
 	private S3Service s3Service;
 	
 	@Autowired
-	private ClienteService clienteService;
-	
-	@Autowired
 	private ImageService imageService;
 	
 	@Value("${img.prefix.client.profile}")
 	private String prefix;
+	
+	@Value("${img.profile.size}")
+	private Integer size;
 	
 	public Cliente find(Integer id) {
 		
@@ -130,6 +130,10 @@ public class ClienteService {
 		}
 		
 		BufferedImage jpgImage = imageService.getJpgImageFromFile(multipartFile);
+		jpgImage =  imageService.cropSquare(jpgImage);
+		jpgImage = imageService.resize(jpgImage, size);
+		
+		
 		String fileNameString = prefix + user.getId() + ".jpg";
 		
 		return s3Service.uploadFile(imageService.getInputStream(jpgImage, "jpg"), fileNameString, "image");
